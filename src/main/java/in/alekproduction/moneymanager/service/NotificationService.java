05c66f2e82815180ba7db7f03cdc62c3af37c181
Sasha Turchanskiy <turchanskiy777@gmail.com>
2025-12-17 12:38:28 +0200

    finished working with notifications service

--- src/main/java/in/alekproduction/moneymanager/service/NotificationService.java
+++ src/main/java/in/alekproduction/moneymanager/service/NotificationService.java
@@ -1,15 +1,19 @@
 package in.alekproduction.moneymanager.service;
 
+import in.alekproduction.moneymanager.dto.ExpenseDto;
 import in.alekproduction.moneymanager.enitity.ProfileEntity;
+import in.alekproduction.moneymanager.repository.ExpenseRepo;
 import in.alekproduction.moneymanager.repository.ProfileRepo;
 import lombok.RequiredArgsConstructor;
 
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -40,5 +44,40 @@ public class NotificationService {
                      "Money Manager Team";
                 emailService.sendEmail(profile.getEmail(), "Daily Income/Expense Reminder", body);
          }
+            log.info("Daily income/expense reminder job completed");
     }
+
+    //@Scheduled(cron = "0 * * * * ?", zone = "UTC")
+    @Scheduled(cron = "0 0 23 * * ?", zone = "UTC")
+    public void sendDailyExpenseSummary(){
+        log.info("Job started: Sending daily expense summary emails to all users.");
+        List<ProfileEntity> profiles = profileRepo.findAll();
+        for (ProfileEntity profile : profiles){
+            List<ExpenseDto> todayExpenses = expenseService.getExpensesByIdAndDate(profile.getId(), LocalDate.now());
+            if (!todayExpenses.isEmpty()){
+                StringBuilder table = new StringBuilder();
+                table.append("<table border='1' style='border-collapse: collapse;'>");
+                table.append("<tr style='background-color: #f2f2f2;'><th style='padding: 8px;'>Name</th><th style='padding: 8px;'>Category</th><th style='padding: 8px;'>Amount</th></tr>");
+                int i = 0;
+                for (ExpenseDto expense : todayExpenses) {
+                    table.append("<tr>");
+                    table.append("<td style='border: 1px solid #ddd; padding: 8px;'>").append(i++).append("</td>");
+                    table.append("<td style='border: 1px solid #ddd; padding: 8px;'>").append(expense.getName()).append("</td>");
+                    table.append("<td style='border: 1px solid #ddd; padding: 8px;'>").append(expense.getAmount()).append("</td>");
+                    table.append("<td style='border: 1px solid #ddd; padding: 8px;'>").append(expense.getCategoryId() != null ? expense.getCategoryName() : "N/A").append("</td>");
+                    table.append("</tr>");
+                }
+                table.append("</table>");
+                String body = "Dear " + profile.getFullName() + ",<br><br>" +
+                        "Here is the summary of your expenses for today:<br><br>" +
+                        table.toString() +
+                        "<br>Thank you for using our Money Manager application!<br><br>" +
+                        "Best regards,<br>" +
+                        "Money Manager Team";
+                emailService.sendEmail(profile.getEmail(), "Daily Expense Summary", body);
+            }
+        }
+            log.info("Job completed: Daily expense summary emails sent to all users.");
+    }
+
 }
